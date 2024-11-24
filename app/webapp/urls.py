66c4404c6929bf8396cb273489d653c61efe2a1b
filipopo <filipmymail@gmail.com>
2024-11-24 12:28:34 +0100

    Set urls to files

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('u/<str:path>', views.u, name='u'),
     path('url/<str:path>', views.url, name='url'),
-    path('url/', views.urls, name='urls'),
+    path('url', views.urls, name='urls'),
     path('link/<str:path>', views.link, name='link'),
     path('link/', views.links, name='links'),
-    path('accounts/register/', views.register, name='register'),
+    path('accounts/register', views.register, name='register'),
 ]
